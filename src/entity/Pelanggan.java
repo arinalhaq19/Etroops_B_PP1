c6f047a2191a8f317a3ce90ac7a7e80ade97591c
Muhamadrizky27 <rizkyers80@gmail.com>
2025-06-08 22:06:12 +0700

    Feat:Perbarui

--- src/entity/Pelanggan.java
+++ src/entity/Pelanggan.java
@@ -1,41 +1,44 @@
 package entity;
-public class Pelanggan {
-  private int idPelanggan;
-    private String nama;
-    private String nomorTelepon;  
-    private String alamat;
 
-    // Konstruktor
-    public Pelanggan(int idPelanggan, String nama, String nomorTelepon, String alamat) {
-        setIdPelanggan(idPelanggan);
-        setNama(nama);
-        setNomorTelepon(nomorTelepon);
-        setAlamat(alamat);
+import java.time.LocalDate;
+
+
+public class Pelanggan {
+    private int idPelanggan;
+    
+    private String nama;
+    private String alamat;
+    private String noTelepon;
+    private LocalDate tanggalBergabung;
+    private String levelMember;
+    private Pelanggan next;
+
+
+public Pelanggan(int idPelanggan,String nama,String alamat,String noTelepon,LocalDate tanggalBergabung,String levelMember){
+    this.idPelanggan = idPelanggan; 
+    this.nama = nama;
+    this.alamat = alamat;
+    this.noTelepon = noTelepon;
+    this.tanggalBergabung = tanggalBergabung;
+    this.levelMember = levelMember;
+    this.next = null;
     }
 
-    // Getter & Setter
+    // Getter dan Setter
     public int getIdPelanggan() {
         return idPelanggan;
-    }
-
+    }       
+    
     public void setIdPelanggan(int idPelanggan) {
         this.idPelanggan = idPelanggan;
     }
 
     public String getNama() {
         return nama;
-    }
+    }               
 
     public void setNama(String nama) {
         this.nama = nama;
-    }
-
-    public String getNomorTelepon() {
-        return nomorTelepon;
-    }
-
-    public void setNomorTelepon(String nomorTelepon) {
-        this.nomorTelepon = nomorTelepon;
     }
 
     public String getAlamat() {
@@ -46,15 +49,47 @@ public class Pelanggan {
         this.alamat = alamat;
     }
 
-    
-
-    @Override
-    public String toString() {
-        return "\n============ Pelanggan ID: " + idPelanggan + " ===========\n" +
-               "Nama           : " + nama + "\n" +
-               "Nomor Telepon  : " + nomorTelepon + "\n" +
-            "Alamat         : " + alamat + "\n";
+    public String getNoTelepon() {
+        return noTelepon;
     }
+
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+
+    public LocalDate getTanggalBergabung() {
+        return tanggalBergabung;
+    }
+
+    public void setTanggalBergabung(LocalDate tanggalBergabung) {
+        this.tanggalBergabung = tanggalBergabung;
+    }
+
+    public String getlevelMember() {
+        return levelMember;
+    }
+
+    public void setlevelMember(String levelMember) {
+        this.levelMember = levelMember;
+    }
+
+    public Pelanggan getNext() {
+        return next;
+    }
+
+    public void setNext(Pelanggan next) {
+        this.next = next;
+    }
+
+@Override
+public String toString() {
+    return "\nPelanggan\n" +
+            "idPelanggan     : " + idPelanggan + "\n" +
+            "nama            : " + nama + "\n" +
+            "alamat          : " + alamat + "\n" +
+            "noTelepon       : " + noTelepon + "\n" +
+            "tanggalBergabung: " + tanggalBergabung + "\n" +
+            "levelMember     : " + levelMember + "\n";
 }
 
-  
+}
