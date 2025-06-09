78f001affaf3bcd00eb4e17637345b7bb621df5d
rafiasmaulrizal <rafiarizals@gmail.com>
2025-06-09 11:31:50 +0700

    Feat: Tambah Validasi NoTelepon

--- src/util/Validasi.java
+++ src/util/Validasi.java
@@ -6,4 +6,9 @@ public class Validasi {
         return id > 0;
         
     }
+
+    // Validasi nomor telepon: minimal 10 digit dan hanya angka
+    public static boolean validasiTelepon(String telp) {
+        return telp != null && telp.matches("\\d{10,}");
+    }
 }
